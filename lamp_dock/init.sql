0bc2e2165b33b6ca22d3a95c99d68f71a5ecd136
nin9nin9nin <nin9nin9nin@gmail.com>
2022-10-13 20:39:16 +0900

    テーブル設計

--- lamp_dock/init.sql
+++ lamp_dock/init.sql
@@ -8,7 +8,32 @@ CREATE TABLE admin (
   update_datetime DATETIME COMMENT 'レコードの更新日',
   primary key(admin_id)
 );
-       
+
+ -- イベント管理
+CREATE TABLE  events (
+  event_id int(11) NOT NULL COMMENT 'イベントID' AUTO_INCREMENT,
+  event_name varchar(64) NOT NULL COMMENT 'イベント名' COLLATE utf8_general_ci,
+  description text COMMENT 'イベント説明' COLLATE utf8_general_ci,
+  event_date varchar(64) NOT NULL COMMENT '開催期間' COLLATE utf8_general_ci,
+  event_tag int(11) NOT NULL default 0 COMMENT 'ステータス（0:ポップアップ、1:イベント）',
+  event_svg varchar(128) COMMENT 'svg画像',
+  event_png varchar(128) COMMENT 'png画像',
+  img1 varchar(128) COMMENT '画像1',
+  img2 varchar(128) COMMENT '画像2',
+  img3 varchar(128) COMMENT '画像3',
+  img4 varchar(128) COMMENT '画像4',
+  img5 varchar(128) COMMENT '画像5',
+  img6 varchar(128) COMMENT '画像6',
+  img7 varchar(128) COMMENT '画像7',
+  img8 varchar(128) COMMENT '画像8',
+  img9 varchar(128) COMMENT '画像9',
+  img10 varchar(128) COMMENT '画像10',
+  status int(11) NOT NULL default 0 COMMENT 'ステータス（0:非公開、1:公開）',
+  create_datetime DATETIME COMMENT 'レコードの作成日',
+  update_datetime DATETIME COMMENT 'レコードの更新日',
+  primary key(event_id)
+);
+
 -- カテゴリー管理
 CREATE TABLE  categorys (
   category_id int(11) NOT NULL COMMENT 'カテゴリーID' AUTO_INCREMENT,
@@ -24,49 +49,30 @@ CREATE TABLE  categorys (
 CREATE TABLE  brands (
   brand_id int(11) NOT NULL COMMENT 'ブランドID' AUTO_INCREMENT,
   brand_name varchar(64) NOT NULL COMMENT 'ブランド名' COLLATE utf8_general_ci,
-  category_id int(11) NOT NULL COMMENT 'カテゴリーID（マンスリー）',
   description text COMMENT 'ブランド説明' COLLATE utf8_general_ci,
+  brand_logo varchar(128) COMMENT 'ブランドロゴ',
   brand_hp text COMMENT 'ブランドHP' COLLATE utf8_general_ci,
-  brand_link1 text COMMENT 'ブランドLINK1' COLLATE utf8_general_ci,
-  brand_link2 text COMMENT 'ブランドLINK2' COLLATE utf8_general_ci,
-  brand_link3 text COMMENT 'ブランドLINK3' COLLATE utf8_general_ci,
-  brand_link4 text COMMENT 'ブランドLINK4' COLLATE utf8_general_ci,
+  brand_instagram text COMMENT 'ブランドinstagram' COLLATE utf8_general_ci,
+  brand_twitter text COMMENT 'ブランドtwitter' COLLATE utf8_general_ci,
+  brand_facebook text COMMENT 'ブランドfacebook' COLLATE utf8_general_ci,
+  brand_youtube text COMMENT 'ブランドyoutube' COLLATE utf8_general_ci,
+  brand_line text COMMENT 'ブランドline' COLLATE utf8_general_ci,
   phone_number varchar(11) COMMENT '電話番号' COLLATE utf8_general_ci,
   email varchar(128) COMMENT 'メールアドレス' COLLATE utf8_general_ci,
   address varchar(64) COMMENT '住所' COLLATE utf8_general_ci,
   status int(11) NOT NULL default 0 COMMENT 'ステータス（0:非公開、1:公開）',
   create_datetime DATETIME COMMENT 'レコードの作成日',
   update_datetime DATETIME COMMENT 'レコードの更新日',
-  -- foreign key(category_id) references categorys (category_id) on delete set null on update cascade,
   primary key(brand_id)
-);
-
--- ショップ管理
-CREATE TABLE  shops (
-  shop_id int(11) NOT NULL COMMENT 'ショップID' AUTO_INCREMENT,
-  shop_name varchar(64) NOT NULL COMMENT 'ショップ名' COLLATE utf8_general_ci,
-  description text COMMENT 'ショップ説明' COLLATE utf8_general_ci,
-  shop_hp text COMMENT 'ショップHP' COLLATE utf8_general_ci,
-  shop_link1 text COMMENT 'ショップLINK1' COLLATE utf8_general_ci,
-  shop_link2 text COMMENT 'ショップLINK2' COLLATE utf8_general_ci,
-  shop_link3 text COMMENT 'ショップLINK3' COLLATE utf8_general_ci,
-  shop_link4 text COMMENT 'ショップLINK4' COLLATE utf8_general_ci,
-  phone_number varchar(11) COMMENT '電話番号' COLLATE utf8_general_ci,
-  email varchar(128) COMMENT 'メールアドレス' COLLATE utf8_general_ci,
-  address varchar(64) COMMENT '住所' COLLATE utf8_general_ci,
-  status int(11) NOT NULL default 0 COMMENT 'ステータス（0:非公開、1:公開）',
-  create_datetime DATETIME COMMENT 'レコードの作成日',
-  update_datetime DATETIME COMMENT 'レコードの更新日',
-  primary key(shop_id)
 );
 
 -- 商品
 CREATE TABLE items (
   item_id int(11) NOT NULL COMMENT '商品ID' AUTO_INCREMENT,
   item_name varchar(64) NOT NULL COMMENT '商品名' COLLATE utf8_general_ci,
-  category_id int(11) NOT NULL default 0 COMMENT 'カテゴリーID（ジャンル）',
-  brand_id int(11) NOT NULL default 0 COMMENT 'ブランドID（マンスリー）',
-  shop_id int(11) NOT NULL default 0 COMMENT 'ショップID',
+  category_id int(11) NOT NULL default 0 COMMENT 'カテゴリーID',
+  brand_id int(11) NOT NULL default 0 COMMENT 'ブランドID',
+  event_id int(11) NOT NULL default 0 COMMENT 'イベントID',
   price int(11) NOT NULL COMMENT '値段',
   description text COMMENT '商品説明' COLLATE utf8_general_ci,
   icon_img varchar(128) NOT NULL COMMENT 'アイコン画像',
@@ -86,7 +92,7 @@ CREATE TABLE items (
   update_datetime DATETIME COMMENT 'レコードの更新日',
   -- foreign key(brand_id) references brands (brand_id) on delete set null on update cascade,
   -- foreign key(category_id) references categorys (category_id) on delete set null on update cascade,
-  -- foreign key(shop_id) references shops (shop_id) on delete set null on update cascade,
+  -- foreign key(event_id) references shops (event_id) on delete set null on update cascade,
   primary key(item_id)
 );
 -- int(11)の11は、カラムの表示幅であり、2147483647まで格納が可能。
@@ -106,12 +112,12 @@ CREATE TABLE stocks (
 CREATE TABLE users (
   user_id int(11) NOT NULL COMMENT 'ユーザーID' AUTO_INCREMENT,
   user_name varchar(128) NOT NULL unique COMMENT 'ユーザー名' COLLATE utf8_general_ci,
+  email varchar(128) NOT NULL COMMENT 'メールアドレス' COLLATE utf8_general_ci,
   password varchar(255) NOT NULL COMMENT 'パスワード' COLLATE utf8_general_ci,
   create_datetime DATETIME COMMENT 'レコードの作成日',
   update_datetime DATETIME COMMENT 'レコードの更新日',
   primary key(user_id)
 );
-ALTER TABLE users ADD email varchar(128) NOT NULL COMMENT 'メールアドレス' COLLATE utf8_general_ci;
 
 -- お気に入り
 CREATE TABLE favorites (
@@ -120,10 +126,9 @@ CREATE TABLE favorites (
   item_id int(11) COMMENT '商品ID',
   create_datetime DATETIME COMMENT 'レコードの作成日',
   update_datetime DATETIME COMMENT 'レコードの更新日',
-  foreign key(user_id) references users (user_id) on delete set null on update cascade,
-  foreign key(item_id) references items (item_id) on delete set null on update cascade,
   primary key(favorite_id)
 );
+
 -- 顧客
 CREATE TABLE customers (
   customer_id int(11) NOT NULL COMMENT '顧客ID' AUTO_INCREMENT,
@@ -144,6 +149,7 @@ CREATE TABLE customers (
   foreign key(user_id) references users (user_id) on delete set null on update cascade,
   primary key(customer_id)
 );
+
 -- 注文管理
 CREATE TABLE orders (
   order_id int(11) NOT NULL COMMENT '注文ID' AUTO_INCREMENT,
@@ -152,9 +158,9 @@ CREATE TABLE orders (
   enabled boolean NOT NULL default true COMMENT '有効',
   create_datetime DATETIME COMMENT 'レコードの作成日',
   update_datetime DATETIME COMMENT 'レコードの更新日',
-  foreign key(customer_id) references customers (customer_id) on delete set null on update cascade,
   primary key(order_id)
 );
+
 -- 注文詳細
 CREATE TABLE order_detail (
   order_id int(11) NOT NULL COMMENT '注文ID',
@@ -164,6 +170,7 @@ CREATE TABLE order_detail (
   update_datetime DATETIME COMMENT 'レコードの更新日',
   primary key(order_id, item_id)
 );
+
 -- カート
 CREATE TABLE carts (
   cart_id int(11) NOT NULL COMMENT 'カートID' AUTO_INCREMENT,
@@ -172,9 +179,9 @@ CREATE TABLE carts (
   cart_date datetime NOT NULL COMMENT '追加日',
   create_datetime DATETIME COMMENT 'レコードの作成日',
   update_datetime DATETIME COMMENT 'レコードの更新日',
-  foreign key(user_id) references users (user_id) on delete set null on update cascade,
   primary key(cart_id)
 );
+
 -- カート詳細
 CREATE TABLE cart_detail (
   cart_id int(11) NOT NULL COMMENT 'カートID',
